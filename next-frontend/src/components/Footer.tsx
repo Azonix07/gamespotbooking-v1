db1fb0bdbdfa243cbf05518d66e0c36489b672de
Escanor360 <abhijith@benagon.com>
2026-02-19 17:59:55 +0530

    feat: make CarbonCrew Technologies clickable link to carboncrew.in

--- next-frontend/src/components/Footer.tsx
+++ next-frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = ({ transparent = false }: { transparent?: boolean }) => {
     <footer className={`site-footer ${transparent ? 'footer-transparent' : ''}`}>
       <div className="footer-content">
         <p className="copyright-text">
-          © Copyright 2026 CarbonCrew Technologies. All Rights Reserved.
+          © Copyright 2026 <a href="https://carboncrew.in" target="_blank" rel="noopener noreferrer" className="carboncrew-link">CarbonCrew Technologies</a>. All Rights Reserved.
         </p>
       </div>
     </footer>
